9a46782ad8d2c68484b45f98dbbf4607600cf4ab
spgingras <simon.pierre.gingras@arcbees.com>
2014-04-01 00:01:05 -0400

    Basic test harness

---
+++ src/test/java/com/google/gwt/resources/client/TestResources.java
@@ -0,0 +1,10 @@
+package com.google.gwt.resources.client;
+
+public interface TestResources extends ClientBundle {
+    public interface Styles extends GssResource {
+      String someClass();
+    }
+
+    @Source("sample.gss")
+    public Styles style();
+}
