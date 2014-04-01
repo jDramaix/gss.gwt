f2010d19de8d9294ca4642225f9263a254bb784b
spgingras <simon.pierre.gingras@arcbees.com>
2014-04-01 00:07:08 -0400

    code style

--- src/test/java/com/google/gwt/resources/client/TestResources.java
+++ src/test/java/com/google/gwt/resources/client/TestResources.java
@@ -1,10 +1,10 @@
 package com.google.gwt.resources.client;
 
 public interface TestResources extends ClientBundle {
-    public interface Styles extends GssResource {
-      String someClass();
-    }
+  public interface Styles extends GssResource {
+    String someClass();
+  }
 
-    @Source("sample.gss")
-    public Styles style();
+  @Source("sample.gss")
+  public Styles style();
 }
