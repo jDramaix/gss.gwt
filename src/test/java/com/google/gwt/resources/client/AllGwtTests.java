0afe766ec6e7b329d1b6aa3fd6c68cba0ea83bd0
Daniel Kurka <dankurka@google.com>
2014-08-07 17:18:00 +0200

    Add a suite for GWT tests.

---
+++ src/test/java/com/google/gwt/resources/client/AllGwtTests.java
@@ -0,0 +1,16 @@
+package com.google.gwt.resources.client;
+
+import com.google.gwt.junit.tools.GWTTestSuite;
+
+import junit.framework.Test;
+
+/**
+ * Suite for all GWT tests.
+ */
+public class AllGwtTests extends GWTTestSuite {
+
+  public static Test suite() {
+    GWTTestSuite suite = new GWTTestSuite();
+    return suite;
+  }
+}
